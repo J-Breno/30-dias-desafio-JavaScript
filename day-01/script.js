b58a99c25ff903f827518428ea5cf536689358a0
J-Breno <joao.breno85@hotmail.com>
2024-02-01 14:03:22 -0300

    Iniciando o dia 1 de 30 dias de desafios de javascript, sempre que possivel irei refatorar o desafio

---
+++ day-01/script.js
@@ -0,0 +1,39 @@
+(function(win, doc) {
+    
+    'use strict';
+    
+    /*
+    Desafio: Ajudando a academia
+    
+    A academia de Oficiais da Federação precisa da sua ajuda prara criar um programa que automatize os cálculos das médias de alunos e turmas para melhorar acompanhar os seus desempenhos. Este programa será incorporado em seu sistema principal, portanto você precisa apenas desenvolver a função que calcula a média. No entanto, a função deve ser capaz de funcionar com qualquer número de alunos ou turmas.
+    
+    Escreva uma função que recebe um número qualquer de números inteiros como agrupamentos e retorne a média aritimética entre eles.
+    
+    média aritimética = soma de todos os números, dividida pelo total de números = 5 + 10 + 6 / 3 = 7
+    
+    Exemplo: 
+    - Entrada(10, 9, 6, 8, 9, 1, 5, 7)
+    - Saída: 6.875
+    
+    - Entrada(2, 5, 7, 1, -2)
+    - Saída: 2.6
+    
+    - Entrada(10, 10, 10, 10, 9)
+    - Saída: 9.8
+    
+    - Entrada(25, 75)
+    - Saída: 50
+    */
+    
+
+    function calculateAverage(...numbers) {
+        const sum = numbers.reduce(function(accumulate, valorAtual) {
+            return accumulate + valorAtual;
+        })
+        
+        const media = sum / numbers.length
+        return `A média foi: ${media}`;
+    }
+   
+    console.log(calculateAverage( 10, 9, 6, 8, 9, 1, 5, 7 ));
+})();
