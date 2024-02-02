f7de2e3b73a0777b9374a62ff3bdef61868d7d07
J-Breno <joao.breno85@hotmail.com>
2024-02-02 07:35:39 -0300

    terminei o desafio do dia 4, possivelmente irei refatorar ele depois

---
+++ day-04/script.js
@@ -0,0 +1,34 @@
+(function() {
+
+    'use strict';
+
+    /**
+     * 4. A quipe encarregada de aperfeiçoar o sistema de navegação das naves da Federeção precisa que você os ajude criando uma nova funcionalidade. Essa funcionalidade consiste em calcular a elevação quadrática individual de um determinado número. Para realizar esse cálculo você deve elevar ao quadrado cada algoritmo do número em questão e concatenar os resultdados em um único número;
+     * 
+     * Escreva uma função que recebe um número inteiro qualquer, ele ao quadrado cada um de seus algarismos e depois concatene o resultado retornando um único número inteiro;
+     * 
+     * Entrada: 3514
+     * Saída: 925116
+     * Entrada: 24
+     * Saída: 416
+     * Entrada: 745821698
+     * Saída: 4916256441368164
+     * 
+     */
+
+        function squaredNumber(...numbers) {
+            let valueSqaured = 0;
+            let newValue = [];
+            let value = '';
+            numbers.forEach(function(item, index) {
+                newValue = item.toString().split('');
+            })
+            newValue.forEach(function(item, index) {
+                value += item * item;
+            })
+            return value;
+        }
+        console.log(squaredNumber(745821698));
+        console.log(squaredNumber(3514));
+        console.log(squaredNumber(24));
+})();
